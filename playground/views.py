7aeb39a128cdd3d6013a16caf315908b259c163f
Mohamed-Waiel-Shikfa <mshikfa@andrew.cmu.edu>
2023-12-08 16:49:04 +0300

    changed storefront to main

--- playground/views.py
+++ playground/views.py
@@ -5,3 +5,6 @@ from django.http import HttpResponse
 # Create your views here.
 def say_hello(request):
     return render(request, "hello.html", {"names": ["hello",2,3, 4]})
+
+def say_playgroundpage(request):
+    return render(request, "hello.html", {"names": ["playground",2,3, 4]})
