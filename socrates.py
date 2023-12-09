4a0d3b758d75675f786743a92ea1c6f07b73ef8d
Ebil Jacob <ebiljacob99@gmail.com>
2023-12-09 11:35:33 +0300

    Added history to the chat

--- socrates.py
+++ socrates.py
@@ -2,7 +2,11 @@ import chromadb
 from dotenv import load_dotenv
 from langchain.embeddings.gpt4all import GPT4AllEmbeddings
 from langchain.chat_models import AzureChatOpenAI
-from langchain.schema import HumanMessage, SystemMessage
+from langchain.schema import HumanMessage, SystemMessage, AIMessage
+from langchain.prompts import ChatPromptTemplate, HumanMessagePromptTemplate, MessagesPlaceholder, AIMessagePromptTemplate
+from langchain.chains import LLMChain
+from langchain.memory import ConversationBufferMemory
+from langchain.prompts import PromptTemplate
 from os import environ
 
 load_dotenv()
@@ -11,6 +15,8 @@ COLLECTION_NAME = "philosophy"
 
 gpt4allembed = GPT4AllEmbeddings()
 chroma_client = chromadb.PersistentClient(path=CHROMA_DATA_PATH)
+model = AzureChatOpenAI(azure_endpoint=environ['AZURE_OPENAI_ENDPOINT'], api_key=environ['AZURE_OPENAI_API_KEY'], openai_api_version=environ['OPENAI_API_VERSION'], model=environ['MODEL_NAME'])
+memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
 
 def query_for_documents(query: str, n_results: int = 10, text_only=True) -> list[str]:
     global gpt4allembed, chroma_client
@@ -32,20 +38,38 @@ def query_for_documents(query: str, n_results: int = 10, text_only=True) -> list
         return query_results
 
 def generate_response(query: str, n_results: int = 10, return_documents=False) -> str:
+    global model
 
     documents = query_for_documents(query, n_results=n_results)
 
-    model = AzureChatOpenAI(azure_endpoint=environ['AZURE_OPENAI_ENDPOINT'], api_key=environ['AZURE_OPENAI_API_KEY'], openai_api_version=environ['OPENAI_API_VERSION'], model=environ['MODEL_NAME'])
+    prompt = ChatPromptTemplate.from_messages(
+        [
+            SystemMessage(content="You are a friendly chatbot that answers philosophical queries from the user."),
+            MessagesPlaceholder(
+                variable_name="chat_history"
+            ),
+            *[SystemMessage(
+                content=doc
+            ) for doc in documents],
+            SystemMessage(content="Use only the previous messages to have a conversation with the user."),
+            HumanMessagePromptTemplate.from_template(
+                "{human_input}"
+            )
+        ]
+    )
 
-    messages = [SystemMessage(content='use only information from the following texts to answer the query from the user'), SystemMessage(content='\n'.join(documents)), HumanMessage(content=query)]
+    chat_llm_chain = LLMChain(
+        llm=model,
+        prompt=prompt,
+        memory=memory
+    )
 
-    result = model(messages=messages)
-    
-    if return_documents:  
-        return result.content, documents
-    else:
-        return result.content
+    result = chat_llm_chain.predict(human_input=query)
+
+    return result
     
 if __name__ == "__main__":
     print('-------------------------------------------------------------------')
-    print(generate_response('Does language modify thoughts?'))
+    print(generate_response("Does language modify thought?", return_documents=True))
+    print('-------------------------------------------------------------------')
+    print(generate_response("How does this impact my thought?", return_documents=True))
